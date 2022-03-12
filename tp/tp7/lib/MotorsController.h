77eb6423e209af4dcd91e133a5943db76215c4aa
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-12 17:03:35 -0500

    Declared MotorsController methods and attributes

--- tp/tp7/lib/MotorsController.h
+++ tp/tp7/lib/MotorsController.h
@@ -2,5 +2,16 @@
 #include <avr/interrupt.h>
 
 class MotorsController {
+    public:
+        MotorsController();
+        ~MotorsController() = default;
 
+        void adjustLeftMotorSpeed(uint8_t percentage);
+        void adjustRightMotorSpeed(uint8_t percentage);
+
+    private:
+        uint8_t leftPercentage;
+        uint8_t rightPercentage;
+
+        void initialization();
 };
