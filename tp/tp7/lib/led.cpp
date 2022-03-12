6b05392a219cbe69b2129b63702dd139ca1c9b5e
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-12 15:45:34 -0500

    Formatted led files

--- tp/tp7/lib/led.cpp
+++ tp/tp7/lib/led.cpp
@@ -1,29 +1,25 @@
-#include <util/delay.h>
 #include "led.h"
 
-led::led(volatile uint8_t* port, uint8_t pin1, uint8_t pin2) : port_(port), pin1_(pin1), pin2_(pin2)
-{
+#include <util/delay.h>
+
+led::led(volatile uint8_t* port, uint8_t pin1, uint8_t pin2) : port_(port), pin1_(pin1), pin2_(pin2) {
 }
 
-void led::setGreen()
-{
+void led::setGreen() {
     *port_ |= (1 << pin1_);
     *port_ &= ~(1 << pin2_);
 }
 
-void led::setRed()
-{
+void led::setRed() {
     *port_ &= ~(1 << pin1_);
     *port_ |= (1 << pin2_);
 }
 
-void led::setOff()
-{
+void led::setOff() {
     *port_ &= ~((1 << pin2_) | (1 << pin1_));
 }
 
-void led::setAmber() 
-{
+void led::setAmber() {
     setGreen();
     _delay_ms(GREEN_AMBER_DELAY);
     setRed();
