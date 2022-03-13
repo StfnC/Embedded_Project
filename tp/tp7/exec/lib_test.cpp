4a29ec11cafdd4a38447709ddd9bacdf370891e2
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-13 16:54:57 -0400

    Added testing of the library

--- tp/tp7/exec/lib_test.cpp
+++ tp/tp7/exec/lib_test.cpp
@@ -1,18 +1,41 @@
+/**
+ * Testing of the library
+ * 
+ * \file lib_test.cpp
+ * \author Lina Belloui, Stefan Cotargasanu, Faneva Rakotoarivony and Justine Sauquet
+ * \date March 13 2022
+ *
+ */
+
 #define F_CPU 8000000
 
+#include <MotorsController.h>
 #include <avr/io.h>
+#include <led.h>
 #include <util/delay.h>
-#include <debug.h>
 
 int main() {
+    DDRA |= (1 << DDA0) | (1 << DDA1);
 
-    uint8_t msg[] = "Hello Debug!\n";
-    uint8_t msgLength = 14;
-    DEBUG_INIT;
+    led del0(&PORTA, DDA0, DDA1);
 
-    while (true) {
-        DEBUG_PRINT_MESSAGE(msg, msgLength);
-    }
-    
+    MotorsController motors;
+
+    motors.setRightPercentage(50);
+    motors.setLeftPercentage(50);
+    del0.setGreen();
+    _delay_ms(2000);
+    motors.changeRightDirection();
+    motors.changeLeftDirection();
+    del0.setRed();
+    _delay_ms(2000);
+    motors.changeRightDirection();
+    motors.changeLeftDirection();
+    del0.setAmber();
+    _delay_ms(2000);
+    motors.setRightPercentage(0);
+    motors.setLeftPercentage(0);
+    del0.setOff();
+
     return 0;
 }
