fe998a04106c080623eb32795a6ea0d0765bdb15
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-03-20 19:20:08 -0400

    Half functionnal Notes class

--- tp/tp9/lib/Notes.cpp
+++ tp/tp9/lib/Notes.cpp
@@ -1,45 +1,24 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
-
-#include <conter.h>
 #include "Notes.h"
 
-//void Notes::notePWM(int note)
-//{
-//    acitf
-//}
-//void Notes::playNote(int note, double temps)
-//{
-//    acitf
-//}
-
-
-const uint8_t partition[] =
-{
-    Do3, Re3, Mi3, Fa3, Sol3, La3, Si3, Do4
+Notes::Notes() : counter_() {
+    
+    counter_.setGenerationMode(GenerationMode::ClearTimerCompare);
+    counter_.setCompareMode(CompareMode::Toggle);
 }
 
-// formule (1+x) = 8000 * note / 2*1024
-
-{
-    CounterInterrupt conter = CounterInterrupt();
-    conter.setGenerationMode(GenerationMode::ClearTimerCompare);
-    conter.setCompareMode(CompareMode::LowLevel);
-
-   // activer compteur .set (CTC) duration a valur obtenur <=> la 
+void Notes::playNote(uint8_t note) {
+    cli();
+    counter_.setGenerationMode(GenerationMode::ClearTimerCompare);
+    counter_.setCompareMode(CompareMode::Toggle);
     
-    for (int i=0, i< sizeof.partition, i++) {
-        uint16_t x = ((8000 * partition[i]) / (2 * 1024));
-        conter.setDuration(x); 
-    }
-    
+    note = note - 45;
+    uint16_t frequency = notes[note];
+    uint16_t ocr = ( ( 8000000 / frequency ) - 2048 ) /  2048; 
+    OCR1A = ocr;
+    sei();
 }
 
-void Notes::offNote()
-{
-    TCNT0 = 0;
-    OCR0A = 0;  
-    TCCR0A = 0;
-    TCCR0B = 0;
+void Notes::stopNote() {
+    counter_.setCompareMode(CompareMode::Off);
+    OCR1A = 0;
 }
---
+++ tp/tp9/lib/operations.h
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+
+namespace Operations {
+    const uint8_t dbt = 0x01;
+    const uint8_t att = 0x02;
+    const uint8_t dal = 0x44;
+    const uint8_t det = 0x45;
+    const uint8_t sgo = 0x48;
+    const uint8_t sar = 0x09;
+    const uint8_t mar1 = 0x60;
+    const uint8_t mar2 = 0x61;
+    const uint8_t mav = 0x62;
+    const uint8_t mre = 0x63;
+    const uint8_t trd = 0x64;
+    const uint8_t trg = 0x65;
+    const uint8_t dbc = 0xC0;
+    const uint8_t fbc = 0xC1;
+    const uint8_t fin = 0xFF;
+} // namespace operations
