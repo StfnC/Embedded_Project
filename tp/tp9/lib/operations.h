748ec713c1c2cec555840750f1027eccb61dfcc0
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-03-21 09:26:23 -0400

    Added operations naemspace

--- tp/tp9/lib/operations.h
+++ tp/tp9/lib/operations.h
@@ -1,17 +1,17 @@
 namespace Operations {
-    uint8_t dbt = 0x01;
-    uint8_t att = 0x02;
-    uint8_t dal = 0x44;
-    uint8_t det = 0x45;
-    uint8_t sgo = 0x48;
-    uint8_t sar = 0x09;
-    uint8_t mar1 = 0x60;
-    uint8_t mar2 = 0x61;
-    uint8_t mav = 0x62;
-    uint8_t mre = 0x63;
-    uint8_t trd = 0x64;
-    uint8_t trg = 0x64; 
-    uint8_t dbc = 0xC0;
-    uint8_t fbc = 0xC1;
-    uint8_t fin = 0xFF;
+    const uint8_t dbt = 0x01;
+    const uint8_t att = 0x02;
+    const uint8_t dal = 0x44;
+    const uint8_t det = 0x45;
+    const uint8_t sgo = 0x48;
+    const uint8_t sar = 0x09;
+    const uint8_t mar1 = 0x60;
+    const uint8_t mar2 = 0x61;
+    const uint8_t mav = 0x62;
+    const uint8_t mre = 0x63;
+    const uint8_t trd = 0x64;
+    const uint8_t trg = 0x65; 
+    const uint8_t dbc = 0xC0;
+    const uint8_t fbc = 0xC1;
+    const uint8_t fin = 0xFF;
 }
