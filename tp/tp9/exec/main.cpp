c0edbabda9f57040dfb5b063cbf2238b6f4b427b
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-21 16:56:26 -0400

    Added more instructions to the test program

--- tp/tp9/exec/main.cpp
+++ tp/tp9/exec/main.cpp
@@ -7,10 +7,16 @@
 #include <usart.h>
 #include <util/delay.h>
 
+#include "RobotProgramDownloader.h"
+
 int main() {
     DEBUG_INIT;
 
     DDRA |= (1 << DDA0) | (1 << DDA1);
+
+    // RobotProgramDownloader programmer;
+
+    // programmer.acceptProgramData();
 
     Interpreter interpreter;
     uint8_t nInstructions = interpreter.getNumberInstructions();
