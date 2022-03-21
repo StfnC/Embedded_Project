129a7b3a09c458e8cd3799d3a3eacfaa77764e7b
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-03-21 15:58:47 -0400

    Tested Notes (not working)

--- tp/tp9/exec/main.cpp
+++ tp/tp9/exec/main.cpp
@@ -1,22 +1,79 @@
 #define F_CPU 8000000
 
 #include <debug.h>
-#include <led.h>
-#include <memoire_24.h>
 #include <usart.h>
 #include <util/delay.h>
-#include <Partition.h>
+#include <Notes.h>              
 
-#include "RobotProgramDownloader.h"
+
+void Partition();
 
 int main() {
-   
-   DDRA = 0XFF;
-   DDRB = 0XFF;
-   DDRD = 0XFF;
+    // DEBUG_INIT;
+    // DDRA = 0XFF;
+    // DDRB = 0XFF;
+    DDRD = 0XFF;
 
-    Partition::partition();
+    DEBUG_PRINT_VARIABLE(0xFF);
 
-    
+    _delay_ms(5000);
+
+    DEBUG_PRINT_VARIABLE(0x0F);
+
+    Partition();
+
     return 0;
-}
+};
+
+
+void Partition() {
+    DEBUG_PRINT_VARIABLE(0xAA);
+    Notes note = Notes();
+    DEBUG_PRINT_VARIABLE(0xBB);
+
+    note.playNote(80); 
+    DEBUG_PRINT_VARIABLE(0xCC);
+    _delay_ms(2000);
+    DEBUG_PRINT_VARIABLE(0xDD);
+    note.stopNote();
+    DEBUG_PRINT_VARIABLE(0xEE); 
+
+
+    // note.playNote(60); 
+    // _delay_ms(600);
+    // note.stopNote();
+
+
+    // note.playNote(62);
+    // _delay_ms(600);
+    // note.stopNote();
+
+    // note.playNote(64);
+    // _delay_ms(1200);
+    // note.stopNote();
+
+    // note.playNote(62);
+    // _delay_ms(1200);
+    // note.stopNote();
+
+    // note.playNote(60);
+    // _delay_ms(600);
+    // note.stopNote();
+
+    // note.playNote(64);
+    // _delay_ms(600);
+    // note.stopNote();
+
+    // note.playNote(62);
+    // _delay_ms(600);
+    // note.stopNote();
+
+    // note.playNote(62);
+    // _delay_ms(600);
+    // note.stopNote();
+
+    // note.playNote(60);
+    // _delay_ms(2400);
+    // note.stopNote();
+
+};
--- tp/tp9/lib/Notes.cpp
+++ tp/tp9/lib/Notes.cpp
@@ -1,24 +1,22 @@
 #include "Notes.h"
+#include <math.h>
 
 Notes::Notes() : counter_() {
     
     counter_.setGenerationMode(GenerationMode::ClearTimerCompare);
-    counter_.setCompareMode(CompareMode::Toggle);
+    counter_.setCompareMode(CompareMode::Off);
+    OCR1A = 0;
 }
 
 void Notes::playNote(uint8_t note) {
-    cli();
     counter_.setGenerationMode(GenerationMode::ClearTimerCompare);
     counter_.setCompareMode(CompareMode::Toggle);
     
-    note = note - 45;
-    uint16_t frequency = notes[note];
-    uint16_t ocr = ( ( 8000000 / frequency ) - 2048 ) /  2048; 
+    uint16_t frequency = 440 * pow(2, (note - 69) / 12);
+    uint16_t ocr = 8000000 / (2 * 1024 * frequency) - 1;
     OCR1A = ocr;
-    sei();
 }
 
 void Notes::stopNote() {
     counter_.setCompareMode(CompareMode::Off);
-    OCR1A = 0;
 }
