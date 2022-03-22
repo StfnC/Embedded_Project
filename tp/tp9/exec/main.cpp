3ee91f64fb5b779e67e2a94021793aa85bc69824
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-03-22 01:54:52 -0400

    Cleaned up main

--- tp/tp9/exec/main.cpp
+++ tp/tp9/exec/main.cpp
@@ -1,64 +1,8 @@
 #define F_CPU 8000000
-#include "RobotProgramDownloader.h"
-#include <util/delay.h>
-#include <usart.h>
-#include <Notes.h>
 
-void Partition();
+#include <avr/io.h>
+#include <util/delay.h>
 
 int main() {
-    DDRD |= 0xFF;
-    usart tr = usart();
-    Notes note = Notes();
-    note.playNote(80);
+    return 0;
 };
-
-
-// void Partition() {
-//     DEBUG_PRINT_VARIABLE(0xAA);
-//     Notes note = Notes();
-//     DEBUG_PRINT_VARIABLE(0xBB);
-
-//     note.playNote(80);
-//     DEBUG_PRINT_VARIABLE(0xCC);
-
-//     return;
-
-//     // note.playNote(60);
-//     // _delay_ms(600);
-//     // note.stopNote();
-
-
-//     // note.playNote(62);
-//     // _delay_ms(600);
-//     // note.stopNote();
-
-//     // note.playNote(64);
-//     // _delay_ms(1200);
-//     // note.stopNote();
-
-//     // note.playNote(62);
-//     // _delay_ms(1200);
-//     // note.stopNote();
-
-//     // note.playNote(60);
-//     // _delay_ms(600);
-//     // note.stopNote();
-
-//     // note.playNote(64);
-//     // _delay_ms(600);
-//     // note.stopNote();
-
-//     // note.playNote(62);
-//     // _delay_ms(600);
-//     // note.stopNote();
-
-//     // note.playNote(62);
-//     // _delay_ms(600);
-//     // note.stopNote();
-
-//     // note.playNote(60);
-//     // _delay_ms(2400);
-//     // note.stopNote();
-
-// };
