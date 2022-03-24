7cb5a41268db8b86f4b1f9628352de4b72699cd4
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-24 15:49:05 -0400

    Deleted Partition.h

--- tp/tp9/exec/main.cpp
+++ tp/tp9/exec/main.cpp
@@ -1,20 +1,15 @@
 #define F_CPU 8000000
 
 #include <BuzzerController.h>
-#include <RobotProgramDownloader.h>
-#include <avr/io.h>
 #include <Interpreter.h>
-#include <util/delay.h>
-
-#include "Partition.h"
+#include <avr/io.h>
 
 int main() {
-    // RobotProgramDownloader program;
-    // program.acceptProgramData();
-
     DDRA |= (1 << DDA0) | (1 << DDA1);
+
     BuzzerController::initBuzzer();
     Interpreter interpreter;
     interpreter.interpretCode();
+
     return 0;
 };
