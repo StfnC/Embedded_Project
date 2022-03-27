f8eb50787a89651af75e1c62372a8b9a36125600
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-27 14:09:12 -0400

    Ran formatting in debug

--- projet/lib/debug.cpp
+++ projet/lib/debug.cpp
@@ -1,6 +1,6 @@
 /**
  * Implementation of the Debug methods
- * 
+ *
  * \file debug.cpp
  * \author Faneva Rakotoarivony and Stefan Cotargasanu
  * \date March 13 2022
@@ -11,22 +11,18 @@
 
 usart Debug::transmitter_;
 
-void Debug::initDebug()
-{
+void Debug::initDebug() {
     transmitter_ = usart();
 }
 
-void Debug::printVariable(uint8_t variable)
-{
+void Debug::printVariable(uint8_t variable) {
     transmitter_.transmit(variable);
 }
 
-void Debug::printMessage(const char message[], uint8_t value)
-{
+void Debug::printMessage(const char message[], uint8_t value) {
     transmitter_.transmitTextMessage(message, value);
 }
 
-void Debug::printMessage(const char message[])
-{
+void Debug::printMessage(const char message[]) {
     transmitter_.transmitTextMessage(message);
 }
