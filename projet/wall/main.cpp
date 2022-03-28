83818c0049c72e84d202ac0d73f91b91aaff3e23
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-28 13:59:38 -0400

    Tested reading from distance sensor

--- projet/wall/main.cpp
+++ projet/wall/main.cpp
@@ -1,3 +1,22 @@
+#include <can.h>
+#include <usart.h>
+
+const uint8_t ADC_POSITION = 2;
+const uint8_t PRECISION_LOSS = 2;
+
+void initialization() {
+    DDRA &= ~(1 << ADC_POSITION);
+}
+
 int main() {
+    can can;
+    usart usart;
+    uint8_t analogReading;
+
+    while (true) {
+        analogReading = static_cast<uint8_t>(can.lecture(ADC_POSITION) >> PRECISION_LOSS);
+        usart.transmitTextMessage("Analog Reading : %d\n", analogReading);
+    }
+    
     return 0;
 }
