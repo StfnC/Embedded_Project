e26b854a56b5566bd9cf00593abfefb30e82b38d
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-03-28 15:32:20 -0400

    Added base for light

--- projet/light/main.cpp
+++ projet/light/main.cpp
@@ -1,3 +1,14 @@
+#include <can.h>
+#include <usart.h>
+
 int main() {
-    return 0;
+    DDRA = 0x00;
+    can conv = can();
+    usart tr = usart();
+    uint8_t lecture;
+
+    while (true) {
+        lecture = static_cast<uint8_t>(conv.lecture(1) >> 2);
+        tr.transmit(lecture);
+    }
 }
