fd9982bb290982985bfcdbdb2410c85dd6d1fe70
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-03-31 15:26:51 -0400

    Implemented basic wall following when at right distance

--- projet/wall/main.cpp
+++ projet/wall/main.cpp
@@ -1,4 +1,5 @@
 #include <can.h>
+#include <MotorsController.h>
 #include <usart.h>
 
 // FIXME: -Transform this into a class
@@ -10,6 +11,7 @@ const uint8_t PRECISION_LOSS = 2;
 // FIXME: Only for testing, will need to change
 const uint8_t FOLLOWING_DISTANCE_THRESHOLD = 71;
 const uint8_t FOLLOWING_DISTANCE_ERROR = 5;
+const uint8_t MOTOR_MULTIPLIER = 3;
 
 void initialization() {
     DDRA &= ~(1 << ADC_POSITION);
@@ -23,10 +25,28 @@ bool isTooCloseFromWall(uint8_t distance) {
     return (distance > FOLLOWING_DISTANCE_THRESHOLD + FOLLOWING_DISTANCE_ERROR);
 }
 
+void creepCloser(MotorsController& motors) {
+    motors.setLeftPercentage(0);
+    motors.setRightPercentage(0);
+}
+
+void backAway(MotorsController& motors) {
+    motors.setLeftPercentage(0);
+    motors.setRightPercentage(0);
+}
+
+void goStraight(MotorsController& motors) {
+    motors.changeLeftDirection(Direction::Forward);
+    motors.changeRightDirection(Direction::Forward);
+    motors.setLeftPercentage(100);
+    motors.setRightPercentage(100);
+}
+
 int main() {
     can can;
     usart usart;
     uint8_t analogReading;
+    MotorsController motors;
 
     while (true) {
         analogReading = static_cast<uint8_t>(can.lecture(ADC_POSITION) >> PRECISION_LOSS);
@@ -34,10 +54,13 @@ int main() {
 
         if (isTooFarFromWall(analogReading)) {
             usart.transmitTextMessage("Too far\n");
+            creepCloser(motors);
         } else if (isTooCloseFromWall(analogReading)) {
             usart.transmitTextMessage("Too close\n");
+            backAway(motors);
         } else {
             usart.transmitTextMessage("Right distance\n");
+            goStraight(motors);
         }
     }
 
