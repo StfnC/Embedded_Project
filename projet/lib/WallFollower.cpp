db3370efd4e8357236314ed3cc5b22fc0b453574
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-04-07 16:39:44 -0400

    Added fullStop method

--- projet/lib/WallFollower.cpp
+++ projet/lib/WallFollower.cpp
@@ -5,6 +5,8 @@
 
 const uint8_t WallFollower::FOLLOWING_DISTANCE_THRESHOLD = 71;
 const uint8_t WallFollower::FOLLOWING_DISTANCE_ERROR = 5;
+const uint8_t WallFollower::NO_WALL_THRESHOLD = 30;
+
 
 void WallFollower::followWall() {
     uint8_t distance = DistanceSensor::getDistance();
@@ -12,10 +14,12 @@ void WallFollower::followWall() {
     usart::transmitTextMessage("Distance : %d\n", distance);
 
     // FIXME: Create an enum for the different distances
-    if (distance < WallFollower::FOLLOWING_DISTANCE_THRESHOLD - WallFollower::FOLLOWING_DISTANCE_ERROR) {
-        getCloser();
+    if (distance < NO_WALL_THRESHOLD) {
+        fullStop();
     } else if (distance > WallFollower::FOLLOWING_DISTANCE_THRESHOLD + WallFollower::FOLLOWING_DISTANCE_ERROR) {
         getAway();
+    } else if (distance < WallFollower::FOLLOWING_DISTANCE_THRESHOLD - WallFollower::FOLLOWING_DISTANCE_ERROR) {
+        getCloser();
     } else {
         goStraight();
     }
@@ -40,4 +44,9 @@ void WallFollower::getAway() {
     MotorsController::changeRightDirection(Direction::Forward);
     MotorsController::setLeftPercentage(25);
     MotorsController::setRightPercentage(75);
+}
+
+void WallFollower::fullStop() {
+    MotorsController::setRightPercentage(0);
+    MotorsController::setLeftPercentage(0);
 }
--- projet/lib/WallFollower.h
+++ projet/lib/WallFollower.h
@@ -12,8 +12,10 @@ class WallFollower {
     private:
         static const uint8_t FOLLOWING_DISTANCE_THRESHOLD;
         static const uint8_t FOLLOWING_DISTANCE_ERROR;
+        static const uint8_t NO_WALL_THRESHOLD;
 
         static void goStraight();
         static void getCloser();
         static void getAway();
+        static void fullStop();
 };
