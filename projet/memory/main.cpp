452e15b4328065803a2b2a81714f1ec3a0529c00
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-04-11 23:02:52 -0400

    Resolve conflict

--- projet/memory/main.cpp
+++ projet/memory/main.cpp
@@ -2,15 +2,28 @@
 #include <counter.h>
 #include <debug.h>
 #include <memoire_24.h>
+#include <debug.h>
+#include <MotorsController.h>
 
-Memoire24CXXX memoire;
-
+Memoire24CXXX memoire();
 ISR(TIMER1_COMPA_vect) {
 
 }
 
-int main() {
-    DEBUG_INIT;
+void writeSpeed() {
+    uint8_t leftSpeed = MotorsController::getLeftPercentage();
     
 }
+
+int main() {
+    DEBUG_INIT;
+    CounterInterrupt counter = CounterInterrupt();
+    MotorsController::initialization();
+    counter.setGenerationMode(GenerationMode::ClearTimerCompare);
+    counter.setCompareMode(CompareMode::Toggle);
+    counter.setDuration(0.02);
+
+
+}
+
 
