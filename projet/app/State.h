1be2efbde95ba9401cb9a31286649056ba2101a8
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-04-16 11:27:27 -0400

    Created states for the robot

---
+++ projet/app/State.h
@@ -0,0 +1,14 @@
+enum class State {
+    INIT,
+    START_RERUN,
+    RERUN,
+    END_RERUN,
+    START_AUTONOMOUS,
+    START_MEMORISING,
+    FOLLOW_WALL,
+    FOLLOW_LIGHT,
+    STOP_MEMORISING,
+    START_U_TURN,
+    U_TURN,
+    END_AUTONOMOUS
+};
