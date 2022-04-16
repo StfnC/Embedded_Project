75fd9a467451281018597b8d5068af50089a911d
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-04-16 06:03:06 -0400

    Working with 255' gp

---
+++ projet/readMem/main.cpp
@@ -0,0 +1,37 @@
+#define F_CPU 8000000
+#include <LightController.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <counter.h>
+#include <usart.h>
+#include <MemoryManager.h>
+#include <MotorsController.h>
+volatile bool gMinuterieExpiree = false;
+volatile int valeurAvantInterruption;
+volatile int valeurApresInterruption;
+volatile int counter = 0;
+
+ISR(TIMER1_COMPA_vect) {
+    MemoryManager::readMemory();
+    // gMinuterieExpiree = true;
+}
+
+
+
+
+int main() {
+    DDRA |= 0x00;
+
+
+    usart::initialization();
+    usart::transmitTextMessage("CALIBRATION LUMIERE AMBIANTE\n");
+    MotorsController::initialization();
+    usart::transmitTextMessage("\nFIN CALIBRATION LUMIERE AMBIANTE\n");
+
+    MemoryManager::initialization();
+    MemoryManager::setIntervalle(255);
+    while (true)
+    {
+    }
+}
