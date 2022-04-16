74591e4f4a2aab135e906fdf4e51669c3f7de266
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-04-16 12:12:01 -0400

    Defined state machine outline

--- projet/app/Robot.cpp
+++ projet/app/Robot.cpp
@@ -3,9 +3,85 @@
 State Robot::currentState_ = State::INIT;
 
 void Robot::init() {
-
 }
 
 void Robot::run() {
-    
+    manageStateMachine();
+}
+
+void Robot::manageStateMachine() {
+    switch (currentState_) {
+        case State::INIT:
+            manageStateInit();
+            break;
+        case State::START_RERUN:
+            manageStateStartRerun();
+            break;
+        case State::RERUN:
+            manageStateRerun();
+            break;
+        case State::END_RERUN:
+            manageStateEndRerun();
+            break;
+        case State::START_AUTONOMOUS:
+            manageStateStartAutonomous();
+            break;
+        case State::START_MEMORIZING:
+            manageStateStartMemorizing();
+            break;
+        case State::FOLLOW_WALL:
+            manageStateFollowWall();
+            break;
+        case State::FOLLOW_LIGHT:
+            manageStateFollowLight();
+            break;
+        case State::STOP_MEMORIZING:
+            manageStateStartMemorizing();
+            break;
+        case State::START_U_TURN:
+            manageStateStartUTurn();
+            break;
+        case State::U_TURN:
+            manageStateUTurn();
+            break;
+        case State::END_AUTONOMOUS:
+            manageStateEndAutonomous();
+            break;
+    }
+}
+
+void Robot::manageStateInit() {
+}
+
+void Robot::manageStateStartRerun() {
+}
+
+void Robot::manageStateRerun() {
+}
+
+void Robot::manageStateEndRerun() {
+}
+
+void Robot::manageStateStartAutonomous() {
+}
+
+void Robot::manageStateStartMemorizing() {
+}
+
+void Robot::manageStateFollowWall() {
+}
+
+void Robot::manageStateFollowLight() {
+}
+
+void Robot::manageStateStopMemorizing() {
+}
+
+void Robot::manageStateStartUTurn() {
+}
+
+void Robot::manageStateUTurn() {
+}
+
+void Robot::manageStateEndAutonomous() {
 }
--- projet/app/Robot.h
+++ projet/app/Robot.h
@@ -10,4 +10,19 @@ class Robot {
 
     private:
         static State currentState_;
+
+        static void manageStateMachine();
+        static void manageStateInit();
+        static void manageStateStartRerun();
+        static void manageStateRerun();
+        static void manageStateEndRerun();
+        static void manageStateStartAutonomous();
+        static void manageStateStartMemorizing();
+        static void manageStateFollowWall();
+        static void manageStateFollowLight();
+        static void manageStateStopMemorizing();
+        static void manageStateStartUTurn();
+        static void manageStateUTurn();
+        static void manageStateEndAutonomous();
+
 };
