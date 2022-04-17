35e6ed3e889fec6936cdbac5ec585c0d1178b8ab
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-04-17 14:35:10 -0400

    Tested ButtonController reliability with an LED

--- projet/button/main.cpp
+++ projet/button/main.cpp
@@ -1,14 +1,18 @@
 #include <ButtonController.h>
 #include <avr/io.h>
-#include <usart.h>
+#include <led.h>
 
 int main() {
-    usart::initialization();
+    led led(DDA0, DDA1);
     ButtonController::init();
 
     while (true) {
-        uint8_t buttonPressed = PIND & (1 << DDD3);
-        usart::transmitTextMessage("Button BREAD : %d\n", ButtonController::wasBreadButtonPressed());
-        usart::transmitTextMessage("Button SMALL : %d\n", ButtonController::wasSmallButtonPressed());
+        if (!ButtonController::wasBreadButtonPressed() && !ButtonController::wasSmallButtonPressed()) {
+            led.setOff();
+        } else if (ButtonController::wasBreadButtonPressed()) {
+            led.setGreen();
+        } else {
+            led.setRed();
+        }
     }
 }
