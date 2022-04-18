dab6a10c5066372c4e43aa32d3c7e45b9c7cf473
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-04-17 23:04:31 -0400

    Formatting

--- projet/lib/RerunManager.h
+++ projet/lib/RerunManager.h
@@ -1,18 +1,18 @@
 #pragma once
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <memoire_24.h>
 #include <MotorsController.h>
-#include "usart.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <memoire_24.h>
 
 class RerunManager {
-public:
-    static void initializationRead();
-    static void initialization();
-    static void setIntervalle(uint16_t duration);
-    static void writeMemory();
-    static void readMemory();
-private:
-    static uint16_t address_;
-    static Memoire24CXXX memory_;
+    public:
+        static void initializationRead();
+        static void initialization();
+        static void setIntervalle(uint16_t duration);
+        static void writeMemory();
+        static void readMemory();
+
+    private:
+        static uint16_t address_;
+        static Memoire24CXXX memory_;
 };
