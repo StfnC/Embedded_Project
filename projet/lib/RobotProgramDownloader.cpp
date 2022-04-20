6637bc9fdc84c66d8c411ed2ee04cdf3673fb54f
Faneva <faneva.rakotoarivony@polymtl.ca>
2022-04-19 22:59:41 -0400

    Fixed Fixme and Magic Numbers in RobotProgramDownloader

--- projet/lib/RobotProgramDownloader.cpp
+++ projet/lib/RobotProgramDownloader.cpp
@@ -27,20 +27,20 @@ void RobotProgramDownloader::writeTotalBytes() {
     uint16_t firstByteOfNumberOfBytes = static_cast<uint16_t>(usart::receive());
     uint16_t secondByteOfNumberOfBytes = static_cast<uint16_t>(usart::receive());
 
-    totalBytes_ |= firstByteOfNumberOfBytes << 0x08 | secondByteOfNumberOfBytes;
+    totalBytes_ |= firstByteOfNumberOfBytes << VALUE_TO_SHIFT_EIGHT_FIRST_BITS | secondByteOfNumberOfBytes;
 
-    memory_.ecriture(0x0000, static_cast<const uint8_t>(totalBytes_ >> 0x08));
-    _delay_ms(5);
+    memory_.ecriture(0x0000, static_cast<const uint8_t>(totalBytes_ >> VALUE_TO_SHIFT_EIGHT_FIRST_BITS));
+    _delay_ms(FIVE_MS_DELAY);
     memory_.ecriture(0x0000 + 1, static_cast<const uint8_t>(totalBytes_));
-    _delay_ms(5);
+    _delay_ms(FIVE_MS_DELAY);
 }
 
 void RobotProgramDownloader::writeProgramToMemory() {
     uint8_t dataBuffer;
 
-    for (uint16_t i = 2; i < totalBytes_; i++) {
+    for (uint16_t i = START_OF_INSTRUCTIONS; i < totalBytes_; i++) {
         dataBuffer = usart::receive();
         memory_.ecriture(0x0000 + i, dataBuffer);
-        _delay_ms(5);
+        _delay_ms(FIVE_MS_DELAY);
     }
 }
--- projet/lib/RobotProgramDownloader.h
+++ projet/lib/RobotProgramDownloader.h
@@ -16,9 +16,12 @@ class RobotProgramDownloader {
 
         void acceptProgramData();
     private:
+        static constexpr uint8_t VALUE_TO_SHIFT_EIGHT_FIRST_BITS = 8;
+        static constexpr uint8_t FIVE_MS_DELAY = 5;
+        static constexpr uint8_t START_OF_INSTRUCTIONS = 2;
         Memoire24CXXX memory_;
         uint16_t totalBytes_;
-
+        
         void writeTotalBytes();
         void writeProgramToMemory();
 };
