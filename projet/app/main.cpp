445a38bcad5f3f65e38eba3fea6d79c1a3e1df0e
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2022-04-20 06:08:50 -0400

    Added header in main

--- projet/app/main.cpp
+++ projet/app/main.cpp
@@ -1,3 +1,38 @@
+/**
+ * Final Project Execution
+ * 
+ * Initializes the Robot and the usart object, then executes the run method of the Robot, which is a state machine
+ * 
+ * \file main.cpp
+ * \author Lina Belloui, Stefan Cotargasanu, Faneva Rakotoarivony and Justine Sauquet
+ * \date April 20th 2022
+ * 
+ * PINOUT:
+ *  Input:
+ *      DistanceSensor:
+ *          -A2
+ *      Photoresistors:
+ *          -A4 (Left)
+ *          -A5 (Right)
+ *      Motherboard Button:
+ *          -D2
+ *      Breadboard Button:
+ *          -D3
+ *  Output:
+ *      LED:
+ *          -A0 (Plug to negative lead)
+ *          -A1 (Plug to positive lead)
+ *      Motors:
+ *          -B3 (Right PWM)
+ *          -B5 (Right Direction)
+ *          -B4 (Left PWM)
+ *          -B7 (Left Direction)
+ *      Buzzer:
+ *          -D4 (Negative lead)
+ *          -D5 (Positive Lead)
+ * 
+ */
+
 #define F_CPU 8000000L
 
 #include <debug.h>
